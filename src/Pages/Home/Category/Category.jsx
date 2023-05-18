32f627a5899334cbcbcbf5835aef5dde09b251e5
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-05-18 10:51:03 +0600

    category

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,6 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Categorycard from "./Categorycard";
 
+const Category = () => {
+  const [categorys, setCategorys] = useState([]);
 
+  useEffect(() => {
+    fetch("category.json")
+      .then((res) => res.json())
+      .then((data) => setCategorys(data));
+  }, []);
+  return (
+    <div>
+      <div className="text-center">
+        <h3 className="text-5xl font-bold">Features</h3>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {
+            categorys.map(category => <Categorycard
+            key={category.id} category={category}></Categorycard>)
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Category;
